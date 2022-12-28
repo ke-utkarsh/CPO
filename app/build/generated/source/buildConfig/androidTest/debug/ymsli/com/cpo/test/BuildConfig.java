5dd93f7dc0ef638491dcdd9b75f02d0f545de5bc
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2022-12-28 18:51:12 +0530

    Create BuildConfig.java

---
+++ app/build/generated/source/buildConfig/androidTest/debug/ymsli/com/cpo/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ymsli.com.cpo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ymsli.com.cpo.test";
+  public static final String BUILD_TYPE = "debug";
+}
