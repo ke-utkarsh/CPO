abd7205cbb8e55790bfd9bf6d299eb69b5956fa4
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2022-12-28 18:43:20 +0530

    Create _ymsli_com_cpo_utils_MyApplication.java

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_ymsli_com_cpo_utils_MyApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "ymsli.com.cpo.utils.MyApplication"
+)
+public final class _ymsli_com_cpo_utils_MyApplication {
+}
