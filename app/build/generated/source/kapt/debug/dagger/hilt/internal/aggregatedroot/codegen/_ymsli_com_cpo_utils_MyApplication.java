7791ebe5e917101fd417fc70ad022741e9647953
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2022-12-28 18:58:11 +0530

    Add files via upload

---
+++ app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_ymsli_com_cpo_utils_MyApplication.java
@@ -0,0 +1,15 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+
+/**
+ * This class should only be referenced by generated code!This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "ymsli.com.cpo.utils.MyApplication",
+    originatingRoot = "ymsli.com.cpo.utils.MyApplication",
+    rootAnnotation = HiltAndroidApp.class
+)
+public class _ymsli_com_cpo_utils_MyApplication {
+}
