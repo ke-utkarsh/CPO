9845273983a1741292b9850d5bdde6064b2d1459
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2022-12-28 18:52:15 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/ymsli/com/cpo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ymsli.com.cpo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ymsli.com.cpo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
